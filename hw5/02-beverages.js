98e34e2a5d8dc6cbee31c94189f21e93bf82988f
supreeth10 <smuddu2@pdx.edu>
2022-11-14 16:02:01 -0800

    pushed hw5 code

--- hw5/02-beverages.js
+++ hw5/02-beverages.js
@@ -1,10 +1,52 @@
 const url =
-  'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+  "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
-let app = document.querySelector('#results');
+const addBookToDOM = (item) => {
+  let element = document.createElement("div");
+  let heading = document.createElement("p");
+  let image = document.createElement("img");
+  element.className = "figure";
+
+  element.id = item.idDrink;
+  image.src = item.strDrinkThumb;
+  image.alt = `An Image of ${item.strDrink}`;
+  heading.textContent = item.strDrink;
+
+  image.style.height = "250px"; //180px
+  image.style.width = "320px"; //300px
+  element.style.margin = "5px";
+  element.style.width = "340px"; //340px
+  element.style.height = "350px"; //250px
+  element.fontWeight = "bold";
+  element.append(image);
+  element.append(heading);
+
+  app.append(element);
+};
+
+let app = document.querySelector("#results");
+app.style.display = "flex";
+app.style.flexWrap = "wrap";
+app.style.flexDirection = "row";
+app.style.justifyContent = "center";
+const loading = document.querySelector("#loading");
 
 const fetchData = (url) => {
-  // Add your code here
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const arr = data.drinks.map((item) => addBookToDOM(item));
+      console.log(data.drinks);
+    })
+    .catch((error) => {
+      console.log(error);
+      let li = document.createElement("div");
+      li.textContent = `An error occured. Please try again.`;
+      app.append(li);
+    })
+    .finally(() => {
+      app.removeChild(loading);
+    });
 };
 
 fetchData(url);
--- hw5/03-beverages.js
+++ hw5/03-beverages.js
@@ -1,6 +1,56 @@
 $(document).ready(function () {
   const url =
-    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
+
+  $("#results").css({
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "center",
+  });
 
   // Add your code here
+  const addBookToDOM = (item) => {
+    $("#results").append(
+      $("<div>")
+        .addClass("figure")
+        .css({
+          margin: "5px",
+          height: "350px", //350px
+          width: "340px", //
+        })
+        .append(
+          $("<img>")
+            .attr("src", `${item.strDrinkThumb}`)
+            .attr("height", "250px") //250px
+            .attr("width", "320px") //320px
+        )
+        .append($("<p>").attr("fontWeight", "bold").text(item.strDrink))
+    );
+  };
+
+  const fetchData = (url) => {
+    $.ajax({
+      type: "GET",
+      url: url,
+      dataType: "JSON",
+      success: (data) => {
+        if (typeof data !== "undefined") {
+          const arr = data.drinks.map((item) => addBookToDOM(item));
+        } else ErrorHandler();
+      },
+      error: (error) => {
+        ErrorHandler();
+      },
+      complete: () => {
+        $("#loading").remove();
+      },
+    });
+  };
+
+  function ErrorHandler() {
+    $("#results").append("<div>").text(`An error occurred. Please try again.`);
+  }
+
+  fetchData(url);
 });
